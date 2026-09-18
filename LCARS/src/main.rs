5a0b08200488a10c0a51ce886da1c8e657ae84d5
nishantSTHA433 <nshrestha3@patriots.uttyler.edu>
2026-09-18 18:48:16 -0500

    Implement basic LCARS application structure

--- LCARS/src/main.rs
+++ LCARS/src/main.rs
@@ -1,1 +1,54 @@
+mod home;
+mod login;
 
+use eframe::egui;
+
+#[derive(PartialEq)]
+pub enum Screen {
+    Login,
+    Home,
+}
+
+pub struct LcarsApp {
+    pub screen: Screen,
+    pub username: String,
+    pub password: String,
+    pub login_message: String,
+}
+
+impl Default for LcarsApp {
+    fn default() -> Self {
+        Self {
+            screen: Screen::Login,
+            username: String::new(),
+            password: String::new(),
+            login_message: String::from("AWAITING USER AUTHENTICATION"),
+        }
+    }
+}
+
+impl eframe::App for LcarsApp {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        match self.screen {
+            Screen::Login => {
+                login::show(self, ctx);
+            }
+
+            Screen::Home => {
+                home::show(self, ctx);
+            }
+        }
+
+        ctx.request_repaint();
+    }
+}
+
+fn main() -> eframe::Result {
+    let options = eframe::NativeOptions::default();
+
+    eframe::run_native(
+        "LCARS System Interface",
+        options,
+        Box::new(|_cc| Ok(Box::new(LcarsApp::default()))),
+    )
+}
